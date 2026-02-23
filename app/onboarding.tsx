17b282d9f092ae537256fa46fdc318870fedde89
Marcus-Fovos <Marcus.fovos@scsstudent.org>
2026-02-23 14:47:08 -0800

    feat: Complete video 23

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,10 +1,12 @@
-import { StyleSheet } from "react-native/types_generated/index";
+import { StyleSheet, Text } from "react-native";
 import { theme } from "@/theme";
 import { useUserStore } from "@/store/userStore";
 import { useRouter } from "expo-router";
 import { PlantlyButton } from "@/components/PlantlyButton";
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
+import { PlantlyImage } from "@/components/PlantlyImage";
+import { View } from "react-native/types_generated/index";
 
 export default function OnboardingScreen() {
   const router = useRouter();
@@ -21,6 +23,13 @@ export default function OnboardingScreen() {
       style={styles.container}
     >
       <StatusBar style="light" />
+      <View>
+        <Text style={styles.heading}>Plantly</Text>
+        <Text style={styles.tagLine}>
+          Keep your plants healthy and hydrated
+        </Text>
+      </View>
+      <PlantlyImage />
       <PlantlyButton title="let me in" onPress={handlePress} />
     </LinearGradient>
   );
@@ -31,6 +40,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: theme.colorWhite,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-evenly",
+  },
+  heading: {
+    fontSize: 42,
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    marginBottom: 12,
+    textAlign: "center",
+  },
+  tagLine: {
+    fontSize: 24,
+    color: theme.colorWhite,
+    textAlign: "center",
   },
 });
