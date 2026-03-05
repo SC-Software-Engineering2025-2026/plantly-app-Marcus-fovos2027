ba373bc9b4dddbc430deeea932e9bccd8bbb55a3
Marcus-Fovos <Marcus.fovos@scsstudent.org>
2026-03-04 17:56:27 -0800

    feat: Complete vidoe 21

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,22 @@
 import { Stack, SplashScreen } from "expo-router";
+import { useEffect } from "react";
+import * as QuickActions from "expo-quick-actions"
+import { Platform } from "react-native";
+import useQuickActionRouting from "expo-quick-actions/router"
 
 SplashScreen.preventAutoHideAsync();
+useQuickActionRouting();
+
+useEffect(() => {
+  QuickActions.setItems([
+    {
+    title: "Add a plant",
+    icon: Platform.OS === "ios" ? "symbol:leaf" : "leaf",
+    id: "0"
+    params: { href: "/new"},
+    },
+  ])
+}, [])
 
 export default function Layout() {
   return (
